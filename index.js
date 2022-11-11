1db084674eb3d02ef7a2cf1236478b611105d935
Yuki (//w//) <indraniko20@gmail.com>
2022-11-12 01:04:12 +0700

    Adding MongoDB into Genre

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const genres = require('./routes/genres')
-const home = require('./routes/home')
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(()=>console.log('Connected to MongoDB...'))
+    .catch(err=> console.error('Could not connect to mongoDB'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api/movies', genres)
-app.use('/', home)
+app.use('/api/genres', genres)
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>console.log(`Listening to ${port}`))
--- routes/genres.js
+++ routes/genres.js
@@ -1,66 +1,60 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 const Joi = require('joi')
 
-const genres = [
-    {
-        id:1,
-        genre:'action'
-    },
-    {
-        id:2,
-        genre:"romance"
-    },
-    {
-        id:3,
-        genre:"horror"
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:50
     }
-]
+}))
 
-router.get('/:id',(req, res)=>{
-    const genre = genres.find(c=>c.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send("MOVIES_ID_NOT_FOUND")
+router.get('/', async (req, res) => {
+    const genres = await Genre.find()
+        .sort('name')
+    res.send(genres)
+})
+
+
+router.get('/:id',async (req, res) => {
+    const genre = await Genre.findById(req.params.id)
+    if (!genre) return res.status(404).send("MOVIES_ID_NOT_FOUND")
     res.send(genre)
 })
 
-router.post('/',(req, res)=>{
-    const {error,result}= schemaValidation(req.body)
-    if(error) return res.status(404).send(error.details[0].message)
+router.post('/',async (req, res) => {
+    const {error, result} = schemaValidation(req.body)
+    if (error) return res.status(404).send(error.details[0].message)
 
-    const movie =
-        {
-            id: genres.length+1,
-            genre:req.body.genre
-        }
-    genres.push(movie)
-    res.send(movie)
+    let genre = new Genre({name: req.body.name})
+    genre = await genre.save()
+    res.send(genre)
 })
 
-router.put('/:id', (req,res)=>{
-    const movie = genres.find(c=>c.id === parseInt(req.params.id))
-    if (!movie) return res.status(404).send("MOVIE_ID_NOT_FOUND")
+router.put('/:id', async (req, res) => {
+    const {error} = schemaValidation(req.body)
+    if (error) return res.status(404).send(error.details[0].message)
 
-    const {error,result}= schemaValidation(req.body)
-
-    if(error) return res.status(404).send(error.details[0].message)
-    movie.genre = req.body.genre
-    res.send(movie)
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {
+        new: true
+    })
+    if (!genre) return res.status(404).send("MOVIE_ID_NOT_FOUND")
+    res.send(genre)
 })
 
-router.delete('/:id',(req, res) => {
-    const movie = genres.find(c=>c.id === parseInt(req.params.id))
-    if (!movie) return res.status(404).send("MOVIE_ID_NOT_FOUND")
-
-    const index = genres.indexOf(movie)
-    genres.splice(index, 1)
+router.delete('/:id',async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id)
+    if (!genre) return res.status(404).send("MOVIE_ID_NOT_FOUND")
     res.send(movie)
-
 })
 
 const schemaValidation = (movie) => {
     const schema= Joi.object(
         {
-            genre: Joi.string().min(3).required()
+            name: Joi.string().min(5).required()
         }
     )
     return schema.validate(movie)
